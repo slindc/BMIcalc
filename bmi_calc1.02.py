e9facfd286a594afeab313cd0b60fc7d8ae44312
slindc <38958301+slindc@users.noreply.github.com>
2018-05-06 12:22:09 -0700

    Chunked and Optimised code by Pascal P

--- bmi_calc1.02.py
+++ bmi_calc1.02.py
@@ -1,6 +1,6 @@
 b=(1/((((((float(input("slindc's ADULT BMI calc\nHight_Feet ") or "0"))*12)+(float(input("Inches ") or "0")))*.0254))**2))*((float(input("Weight in # ") or "1"))/2.2046)
 print("BMI="+str(b))
-if b<=25: input("Congratulations, you are good! ")
+if b<=25: input("Congratulations, you are good!")
 elif 25<b<=30: input("You are overweight! ")
 else: input("You are obese! ")
 
